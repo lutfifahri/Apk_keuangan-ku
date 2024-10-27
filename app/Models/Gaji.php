0620047d2f9a7769c39fe5e4ebe6709d29b075b5
Lutfi Fahri <lfahri959@gmail.com>
2024-10-27 21:33:14 +0700

    fix big model

--- app/Models/Gaji.php
+++ app/Models/Gaji.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Gaji extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'gaji';
+    protected $guarded = ['id'];
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $search = $filters["search"] ?? false;
+
+        $query->when($search, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where("name", "like", "%" . $search . "%");
+            });
+        });
+    }
 }
